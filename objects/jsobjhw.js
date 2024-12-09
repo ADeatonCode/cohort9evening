e8404659a2d4ab4f0d16f95e93273057fb895aa8
Alan <acdelgato@gmail.com>
2024-12-08 22:57:10 -0600

    homework for higher order functions added.

---
+++ objects/jsobjhw.js
@@ -0,0 +1,83 @@
+// Exercise 1:
+
+const dog = {};
+console.log(dog);
+
+// Add properties to the dog object
+
+dog.name = 'Max';
+dog.legs = 4;
+dog.color = 'brown';
+dog.age = 3;
+dog.bark = function () { 
+    return "woof woof!";
+}
+    console.log(dog);
+console.log(dog.bark());
+dog.breed = 'hinz 57';
+dog.getDogInfo = 'Prince Charles Spaniel';
+
+console.log(dog);
+
+// Exercise 2:
+
+const users = {
+    Alex: {
+        email: 'alex@alex.com',
+        skills: ['HTML', 'CSS', 'JavaScript'],
+        age: 20,
+        isLoggedIn: false,
+        points: 30
+    },
+    Asab: {
+        email: 'asab@asab.com',
+        skills: ['HTML', 'CSS', 'JavaScript', 'Redux', 'MongoDB', 'Express', 'React', 'Node'],
+        age: 25,
+        isLoggedIn: false,
+        points: 50
+    },
+    Brook: {
+        email: 'daniel@daniel.com',
+        skills: ['HTML', 'CSS', 'JavaScript', 'React', 'Redux'],
+        age: 30,
+        isLoggedIn: true,
+        points: 50
+    },
+    Daniel: {
+        email: 'daniel@alex.com',
+        skills: ['HTML', 'CSS', 'JavaScript', 'Python'],
+        age: 20,
+        isLoggedIn: false,
+        points: 40
+    },
+    John: {
+        email: 'john@john.com',
+        skills: ['HTML', 'CSS', 'JavaScript', 'React', 'Redux', 'Node.js'],
+        age: 20,
+        isLoggedIn: true,
+        points: 50
+    },
+    Thomas: {
+        email: 'thomas@thomas.com',
+        skills: ['HTML', 'CSS', 'JavaScript', 'React'],
+        age: 20,
+        isLoggedIn: false,
+        points: 40
+    },
+    Paul: {
+        email: 'paul@paul.com',
+        skills: ['HTML', 'CSS', 'JavaScript', 'MongoDB', 'Express', 'React', 'Node'],
+        age: 20,
+        isLoggedIn: false,
+        points: 40
+    }
+
+}
+
+console.log(users);
+let count = Object.keys(users).length;
+console.log(`There are ${count} users in all`);
+for (let user in users) {
+    console.log(`${user}: ${users[user].email}`);
+}
+
